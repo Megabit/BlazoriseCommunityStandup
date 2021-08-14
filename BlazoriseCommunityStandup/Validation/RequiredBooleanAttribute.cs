74b4fcdc164d7ab863f0f8cb1add999ba37d72af
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-08-14 15:22:47 +0100

    Form Page | Data Annoation Boolean Required fix

---
+++ BlazoriseCommunityStandup/Validation/RequiredBooleanAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazoriseCommunityStandup.Validation
+{
+
+    public class RequiredBooleanAttribute : ValidationAttribute
+    {
+        public RequiredBooleanAttribute() : base()
+        {
+
+        }
+
+        public RequiredBooleanAttribute(string errorMessage) : base(errorMessage)
+        {
+
+        }
+        public override bool IsValid( object value )
+            => value != null && bool.TryParse( value.ToString(), out bool result ) && result;
+
+    }
+}
