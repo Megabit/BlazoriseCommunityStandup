79d548d28f3d15042c559262b2805a8c05fb53a6
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-08-14 15:59:27 +0200

    Horizontal product card

--- BlazoriseCommunityStandup.ECommerce/Models/Product.cs
+++ BlazoriseCommunityStandup.ECommerce/Models/Product.cs
@@ -8,8 +8,28 @@ namespace BlazoriseCommunityStandup.ECommerce.Models
 {
     public class Product
     {
+        public Product( string name, string imageUrl, int price, int? discount, bool isNew, bool isHot )
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            Price = price;
+            Discount = discount;
+            IsNew = isNew;
+            IsHot = isHot;
+        }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Price { get; set; }
+
+        public int? Discount { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public bool IsHot { get; set; }
     }
 }
--- BlazoriseCommunityStandup.ECommerce/States/AppState.cs
+++ BlazoriseCommunityStandup.ECommerce/States/AppState.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using BlazoriseCommunityStandup.ECommerce.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazoriseCommunityStandup.ECommerce.States
 {
     public class AppState
     {
+        public bool ProductsAsList { get; set; }
+
+        public List<Product> Products = new List<Product>
+        {
+            new Product("", "img/e-commerce/products/tshirt_red_480x400.webp", 200, 10, true, false),
+            new Product("", "img/e-commerce/products/tshirt_grey_480x400.webp", 475, null, false, false),
+            new Product("", "img/e-commerce/products/tshirt_black_480x400.webp", 350, 20, true, true),
+            new Product("", "img/e-commerce/products/tshirt_green_480x400.webp", 10, null, false, false),
+            new Product("", "img/e-commerce/products/tshirt_red_480x400.webp", 200, 10, true, false),
+            new Product("", "img/e-commerce/products/tshirt_grey_480x400.webp", 475, null, false, false),
+            new Product("", "img/e-commerce/products/tshirt_black_480x400.webp", 350, 20, true, true),
+            new Product("", "img/e-commerce/products/tshirt_green_480x400.webp", 10, null, false, false),
+            new Product("", "img/e-commerce/products/tshirt_red_480x400.webp", 200, 10, true, false),
+            new Product("", "img/e-commerce/products/tshirt_grey_480x400.webp", 475, null, false, false),
+            new Product("", "img/e-commerce/products/tshirt_black_480x400.webp", 350, 20, true, true),
+            new Product("", "img/e-commerce/products/tshirt_green_480x400.webp", 10, null, false, false),
+        };
     }
 }
