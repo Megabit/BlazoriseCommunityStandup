9e46b04a307615be470bc129dc2c7e09e544718c
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-08-14 23:53:21 +0200

    BaseProductCard and use notification service

---
+++ BlazoriseCommunityStandup.ECommerce/Components/BaseProductCard.cs
@@ -0,0 +1,26 @@
+﻿using Blazorise;
+using BlazoriseCommunityStandup.ECommerce.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BlazoriseCommunityStandup.ECommerce.Components
+{
+    public abstract class BaseProductCard : ComponentBase
+    {
+        protected Task AddToCart()
+        {
+            return NotificationService.Success( $"Added {Product.Name} to your cart!" );
+        }
+
+        protected Task AddToWishList()
+        {
+            return NotificationService.Info( $"Added {Product.Name} to Wish List!" );
+        }
+
+        protected int FinalPrice => Product.GetFinalPrice();
+
+        [Inject] private INotificationService NotificationService { get; set; }
+
+        [Parameter] public Product Product { get; set; }
+    }
+}
