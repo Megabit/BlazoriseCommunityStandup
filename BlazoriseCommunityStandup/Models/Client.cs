31065bd7c545e05fa76313960828e71e9b4be6f9
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-08-14 16:18:46 +0100

    Validation Page

---
+++ BlazoriseCommunityStandup/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlazoriseCommunityStandup.Validation;
+
+namespace BlazoriseCommunityStandup.Models
+{
+    public class Client
+    {
+        public long Identification { get; set; }
+        [Required( ErrorMessage = "Please insert your Name" )]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        [RequiredBoolean( "Make sure that you've read the terms and conditions and that you agree to them." )]
+        public bool AcceptedTermsAndConditions { get; set; }
+
+        public string Branch { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
