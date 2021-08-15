e2432133dc50c441a3e04cdca593da21a334851b
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-08-15 15:06:24 +0200

    Validation for account pages

---
+++ BlazoriseCommunityStandup.ECommerce/Models/RecoveryInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazoriseCommunityStandup.ECommerce.Models
+{
+    public class RecoveryInputModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display( Name = "Email" )]
+        public string Email { get; set; }
+    }
+}
---
+++ BlazoriseCommunityStandup.ECommerce/Models/RegisterInputModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazoriseCommunityStandup.ECommerce.Models
+{
+    public class RegisterInputModel
+    {
+        [Required]
+        [DataType( DataType.Text )]
+        [Display( Name = "First Name" )]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType( DataType.Text )]
+        [Display( Name = "Last Name" )]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display( Name = "Email" )]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength( 100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6 )]
+        [DataType( DataType.Password )]
+        [Display( Name = "Password" )]
+        public string Password { get; set; }
+    }
+}
